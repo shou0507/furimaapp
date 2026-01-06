28634bac9b9934a9fab25f7116c922f2ae106b5a
sugiyama sho <shou0507050705@gmail.com>
2026-01-06 20:14:42 +0900

    シーダーを修正

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            ItemSeeder::class,
+            UserSeeder::class,
             CategorySeeder::class,
+            ItemSeeder::class,
         ]);
     }
 }
---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run()
+    {
+        User::factory()->create([
+            'id' => 1,
+            'email' => 'test@example.com',
+        ]);
+    }
+}
